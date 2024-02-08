47ee64cb25f6a8d245bbd8d331c2fc17b0eaf9b8
Wafffle <97140942+Wafffle77@users.noreply.github.com>
2024-02-07 22:27:47 -0600

    oops

--- pam.sh
+++ pam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PAM_RHOST, PAM_RUSER, PAM_SERVICE, PAM_TTY, PAM_USER and PAM_TYPE
 
-/usr/bin/curl "https://discord.com/api/webhooks/1204975377085374474/6brdbDPHm8aDT0OuyVAVqWqNJskvgCudKy9DMjrUaq1FAhKXgdffPXO8iZI6pqU77iH0" -H "Content-Type: application/json" -d "{\"content\": \"PAM_RHOST: $PAM_RHOST\\nPAM_RUSER: $PAM_RUSER\\nPAM_SERVICE: $PAM_SERVICE\\nPAM_TTY: $PAM_TTY\\nPAM_USER: $PAM_USER\\nPAM_TYPE: $PAM_TYPE\"}"
+/usr/bin/curl "$WEBHOOK_URL" -H "Content-Type: application/json" -d "{\"content\": \"PAM_RHOST: $PAM_RHOST\\nPAM_RUSER: $PAM_RUSER\\nPAM_SERVICE: $PAM_SERVICE\\nPAM_TTY: $PAM_TTY\\nPAM_USER: $PAM_USER\\nPAM_TYPE: $PAM_TYPE\"}"
